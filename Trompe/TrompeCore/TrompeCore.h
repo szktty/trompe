39a9aa0861d8894fbefc08aae3cd906967f64c84
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-03-04 02:18:15 +0900

    split an application target into a command line and a library, and add a test target

---
+++ Trompe/TrompeCore/TrompeCore.h
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TrompeCore.
+FOUNDATION_EXPORT double TrompeCoreVersionNumber;
+
+//! Project version string for TrompeCore.
+FOUNDATION_EXPORT const unsigned char TrompeCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrompeCore/PublicHeader.h>
+
+
