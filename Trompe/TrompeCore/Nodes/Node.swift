67dd7422bfc85f9181bea462d0563014ca906961
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-04-10 03:00:45 +0900

    define nodes

--- Trompe/TrompeCore/Nodes/Node.swift
+++ Trompe/TrompeCore/Nodes/Node.swift
@@ -1,7 +1,61 @@
 import Foundation
 
-class Node {
+protocol Node {
     
-    var location: Location?
+    // var location: Location { get }
     
 }
+
+indirect enum SeparatedList<Element, Separator> {
+    
+    case Cons(element: Element, next: SeparatedList<Element, Separator>)
+    case Nil
+    
+}
+
+class Token {
+    
+    var location: Location?
+    var contents: String?
+    
+}
+
+typealias NodeList = SeparatedList<Node, Token>
+
+class UnaryNode: Node {
+
+    var token: Token
+    
+    init(token: Token) {
+        self.token = token
+    }
+    
+}
+
+class EnclosedNode<Contents>: Node {
+    
+    var open: Token?
+    var contents: Contents?
+    var close: Token?
+    
+}
+
+class FunctionNode: Node {
+    
+    var prefix: Node?
+    var arguments: EnclosedNode<NodeList>?
+    
+}
+
+class ReferencePathNode: Node {
+    
+    var path: NodeList?
+    
+}
+
+class BooleanNode: UnaryNode {}
+class StringNode: UnaryNode {}
+class IntegerNode: UnaryNode {}
+class FloatNode: UnaryNode {}
+class TupleNode: EnclosedNode<NodeList> {}
+class ListNode: EnclosedNode<NodeList> {}
