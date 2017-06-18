541cd209709039a5046e72cb7d867ad179cb4ed6
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-06-19 02:50:54 +0900

    impl io.close()

--- lib_io.ml
+++ lib_io.ml
@@ -8,6 +8,13 @@ let prim_open args : Value.t =
   let in_chan = In_channel.create name in
   `Stream (Some in_chan, None)
 
+let prim_close args =
+  check_arity "io.close" 1 args;
+  let in_, out = get_stream args 0 in
+  Option.iter in_ ~f:In_channel.close;
+  Option.iter out ~f:In_channel.close;
+  `Unit
+
 let prim_read_all args =
   check_arity "io.read_all" 1 args;
   match get_stream args 0 with
@@ -18,6 +25,7 @@ let init () =
   Runtime.Spec.(define "io"
                 (* TODO: mode *)
                 +> fun_ "open" Type.Spec.(string @-> stream) "io.open"
+                +> fun_ "close" Type.Spec.(string @-> stream) "io.close"
                 +> fun_ "read_all" Type.Spec.(stream @-> string) "io.read_all"
                 |> end_);
   Runtime.Primitive.add "io.open" prim_open;
