05ea9bb6cf6940099a88b93ec1660a5146e318f4
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2018-01-08 00:13:12 +0900

    length -> len

--- lib_string.ml
+++ lib_string.ml
@@ -1,7 +1,7 @@
 open Base
 open Runtime
 
-let prim_length rt ctx args =
+let prim_len rt ctx args =
   let s = Args.string_exn args 0 in
   (rt, Value.Int (String.length s))
 
@@ -9,9 +9,9 @@ let init rt =
   define rt
     ~name:"string"
     ~attrs:[
-      ("length", Value.Prim "string_length");
+      ("len", Value.Prim "string_len");
     ]
     ~prims:[
-      ("string_length", prim_length, 1)
+      ("string_len", prim_len, 1)
     ]
     ()
