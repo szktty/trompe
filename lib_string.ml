bcc136336e8941d607528f32280f5c23fd003ab9
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2018-01-08 03:17:55 +0900

    impl

--- lib_string.ml
+++ lib_string.ml
@@ -5,13 +5,28 @@ let prim_len rt ctx args =
   let s = Args.string_exn args 0 in
   (rt, Value.Int (String.length s))
 
+let prim_eq rt ctx args =
+  let s1 = Args.string_exn args 0 in
+  let s2 = Args.string_exn args 1 in
+  (rt, Value.Bool (String.equal s1 s2))
+
+let prim_join rt ctx args =
+  let es = Args.list_exn args 0 in
+  let comps = List.map es ~f:Value.string_exn in
+  let sep = Args.string_exn args 1 in
+  rt, Value.String (String.concat comps ~sep)
+
 let init rt =
   define rt
     ~name:"string"
     ~attrs:[
       ("len", Value.Prim "string_len");
+      ("eq", Value.Prim "string_eq");
+      ("join", Value.Prim "string_join");
     ]
     ~prims:[
-      ("string_len", prim_len, 1)
+      ("string_len", prim_len, 1);
+      ("string_eq", prim_eq, 2);
+      ("string_join", prim_eq, 2);
     ]
     ()
