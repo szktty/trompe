12ccd8bc1f7279a4e3d25214aa3d3e7b55d18c21
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2018-03-09 03:25:56 +0900

    ready command

--- cmd/trompe/main.go
+++ cmd/trompe/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
-import "github.com/szktty/trompe"
+import (
+	"flag"
+	"fmt"
+	"github.com/szktty/trompe"
+	"os"
+)
+
+var debugModeOpt = flag.Bool("d", false, "debug mode")
+var verboseModeOpt = flag.Bool("v", false, "verbose mode")
+var versionModeOpt = flag.Bool("version", false, "print version")
 
 func main() {
+	flag.Parse()
+
+	trompe.DebugMode = *debugModeOpt
+	trompe.VerboseMode = *verboseModeOpt
+
+	if *versionModeOpt {
+		fmt.Printf("%s\n", trompe.Version)
+		os.Exit(0)
+	}
+
+	if flag.NArg() < 1 {
+		fmt.Printf("Usage: trompe [options] files\n\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 	fmt.Printf("hello! %d\n", trompe.OpNop)
 }
