bb961f16183ab65459ccb17ed0be1d7fb3c9cc63
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2018-03-18 00:12:59 +0900

    parse a file

--- cmd/trompe/main.go
+++ cmd/trompe/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/szktty/trompe"
+	"github.com/szktty/trompe/parser"
 	"os"
 )
 
 var debugModeOpt = flag.Bool("d", false, "debug mode")
 var verboseModeOpt = flag.Bool("v", false, "verbose mode")
 var versionModeOpt = flag.Bool("version", false, "print version")
+
+type TreeShapeListener struct {
+	*parser.BaseTrompeListener
+}
+
+func NewTreeShapeListener() *TreeShapeListener {
+	return new(TreeShapeListener)
+}
+
+func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
+	//fmt.Println(ctx.GetText())
+}
 
 func main() {
 	flag.Parse()
@@ -34,5 +48,15 @@ func main() {
 		trompe.TestCompiledCodeHelloWorld()
 		trompe.TestCompiledCodeFizzBuzzCompare()
 		trompe.TestCompiledCodeFizzBuzzMatch()
+		os.Exit(0)
 	}
+
+	file := flag.Arg(0)
+	input, _ := antlr.NewFileStream(file)
+	lexer := parser.NewTrompeLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, 0)
+	p := parser.NewTrompeParser(stream)
+	p.BuildParseTrees = true
+	tree := p.Chunk()
+	antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
 }
