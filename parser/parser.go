c806794019598b327fa7bd1ba48fe111cdf124e6
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2018-03-19 01:02:12 +0900

    impl some listeners

--- parser/parser.go
+++ parser/parser.go
@@ -3,68 +3,84 @@ package parser
 import (
 	"fmt"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
-	"github.com/szktty/trompe"
+	. "github.com/szktty/trompe"
 )
 
-type NodeBuilder struct {
-	*BaseTrompeListener
-}
-
-func NewNodeBuilder() *NodeBuilder {
-	return new(NodeBuilder)
-}
-
-func (l *NodeBuilder) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	//fmt.Println(ctx.GetText())
-}
-
-func (l *NodeBuilder) EnterChunk(ctx *ChunkContext) {
-	fmt.Println("enter chunk")
-}
-
-func (l *NodeBuilder) ExitChunk(ctx *ChunkContext) {
-	fmt.Println("exit chunk")
-}
-
-func (l *NodeBuilder) EnterBlock(ctx *BlockContext) {
-	fmt.Println("enter block")
-}
-
-func (l *NodeBuilder) ExitBlock(ctx *BlockContext) {
-	fmt.Println("exit block")
-}
-
-func (l *NodeBuilder) ExitFuncall(ctx *FuncallContext) {
-	fmt.Printf("exit funcall: %s\n", ctx.GetText())
-}
-
-func ptoken(tok antlr.Token) {
-	fmt.Printf("token = line %d, col %d, %d-%d\n", tok.GetLine(), tok.GetColumn(), tok.GetStart(), tok.GetStop())
-}
-
-func toLoc(tok antlr.Token) trompe.Loc {
+func ToLoc(tok antlr.Token) Loc {
 	line := tok.GetLine()
 	col := tok.GetColumn()
 	offset := tok.GetStart()
 	len_ := tok.GetStop() - offset
-	start := trompe.Pos{Line: line, Col: col, Offset: offset}
-	end := trompe.Pos{Line: line, Col: col, Offset: offset + len_}
-	return trompe.Loc{start, end}
+	start := Pos{Line: line, Col: col, Offset: offset}
+	end := Pos{Line: line, Col: col, Offset: offset + len_}
+	return Loc{start, end}
 }
 
-func (l *NodeBuilder) ExitArgs(ctx *ArgsContext) {
-	fmt.Printf("exit args: %s\n", ctx.GetText())
-	o := ctx.GetO()
-	ptoken(o)
-	//loc := toLoc(o)
+type ChunkListener struct {
+	*BaseTrompeListener
+	Node ChunkNode
 }
 
-func Parse(file string) {
+func NewChunkListener() *ChunkListener {
+	return new(ChunkListener)
+}
+
+func (l *ChunkListener) EnterChunk(ctx *ChunkContext) {
+	fmt.Println("enter chunk")
+	blockCtx := ctx.Block()
+	if blockCtx != nil {
+		block := NewBlockListener()
+		blockCtx.EnterRule(block)
+		l.Node = ChunkNode{Block: block.Node}
+	}
+}
+
+func (l *ChunkListener) ExitChunk(ctx *ChunkContext) {
+	fmt.Println("exit chunk")
+}
+
+type BlockListener struct {
+	*BaseTrompeListener
+	Node BlockNode
+}
+
+func NewBlockListener() *BlockListener {
+	return new(BlockListener)
+}
+
+func (l *BlockListener) EnterBlock(ctx *BlockContext) {
+	fmt.Println("enter block")
+	statLen := len(ctx.AllStat()) + 1
+	stats := make([]Node, statLen)
+	for _, statCtx := range ctx.AllStat() {
+		stat := NewStatListener()
+		statCtx.EnterRule(stat)
+		stats = append(stats, stat.Node)
+	}
+	l.Node = BlockNode{Stats: stats}
+}
+
+func (l *BlockListener) ExitBlock(ctx *BlockContext) {
+	fmt.Println("exit block")
+}
+
+type StatListener struct {
+	*BaseTrompeListener
+	Node StatNode
+}
+
+func NewStatListener() *StatListener {
+	return new(StatListener)
+}
+
+func Parse(file string) Node {
 	input, _ := antlr.NewFileStream(file)
 	lexer := NewTrompeLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, 0)
 	p := NewTrompeParser(stream)
 	p.BuildParseTrees = true
 	tree := p.Chunk()
-	antlr.ParseTreeWalkerDefault.Walk(NewNodeBuilder(), tree)
+	listener := NewChunkListener()
+	antlr.ParseTreeWalkerDefault.EnterRule(listener, tree)
+	return &listener.Node
 }
