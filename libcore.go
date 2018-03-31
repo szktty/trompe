1a73e48e4aad4f55fb4b4e58eab21d753b3d95b1
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2018-04-01 00:42:00 +0900

    impl 'id' fun

--- libcore.go
+++ libcore.go
@@ -2,6 +2,13 @@ package trompe
 
 import "fmt"
 
+func LibCoreId(ctx *Context, args []Value, nargs int) (Value, error) {
+	if err := ValidateArity(nil, 1, nargs); err != nil {
+		return nil, err
+	}
+	return args[0], nil
+}
+
 func LibCoreShow(ctx *Context, args []Value, nargs int) (Value, error) {
 	if err := ValidateArity(nil, 1, nargs); err != nil {
 		return nil, err
@@ -11,8 +18,10 @@ func LibCoreShow(ctx *Context, args []Value, nargs int) (Value, error) {
 }
 
 func InstallLibCore() {
+	SetPrim("id", LibCoreId, 1)
 	SetPrim("show", LibCoreShow, 1)
 	m := NewModule(nil, "core")
+	m.AddPrim("id", "id")
 	m.AddPrim("show", "show")
 	AddTopModule(m)
 	AddOpenedModule(m)
